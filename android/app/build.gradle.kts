d136221b0c75427abac556705cceb5e07cb7eb75
wxk <418306613@qq.com>
2025-11-27 21:03:05 +0800

    增加签名功能

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,8 +5,23 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+
+// 添加这两行导入
+import java.io.FileInputStream
+import java.util.Properties
+
+// 修改后的属性加载代码
+val keystorePropertiesFile = rootProject.file("key.properties")
+val keystoreProperties = Properties().apply {
+    if (keystorePropertiesFile.exists()) {
+        // 使用 Kotlin 的 inputStream() 扩展函数替代 FileInputStream
+        load(keystorePropertiesFile.inputStream())
+    }
+}
+
+
 android {
-    namespace = "com.lhht.ai_assistant"
+    namespace = "com.lhht.ai_assistant"  //app包名
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -30,13 +45,24 @@ android {
         versionName = flutter.versionName
     }
 
+    //新增的签名代码
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String? ?: "my-key-alias"
+            keyPassword = keystoreProperties["keyPassword"] as String?
+            storeFile = file(keystoreProperties["storeFile"] as String? ?: "")
+            storePassword = keystoreProperties["storePassword"] as String?
+        }
+    }
+
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
+            //signingConfig = signingConfigs.getByName("debug")
             // 启用R8优化
-            isMinifyEnabled = true
+            isMinifyEnabled = true 
             isDebuggable = false
             isShrinkResources = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
